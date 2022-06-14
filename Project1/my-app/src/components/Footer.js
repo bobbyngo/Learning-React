3d1828e3431e2ead82793750cbccf28d3a89e019
Bao (Bobby) Ngo <ngohuugiabao8980@gmail.com>
2022-06-13 21:29:53 -0400

    Project 1

--- Project1/my-app/src/components/Footer.js
+++ Project1/my-app/src/components/Footer.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import github from '../images/github.jpg';
+import meta from '../images/meta.jpg';
+import instagram from '../images/instagram.png';
 
 export default function Footer() {
   return (
     <footer className='footer'>
       <a href=''>
-        <img className='footer-github' src='../images/github.jpg' />
+        <img className='footer-github' src={github} />
       </a>
       <a href=''>
-        <img className='footer-meta' src='../images/meta.jpg' />
+        <img className='footer-meta' src={meta} />
       </a>
       <a href=''>
-        <img className='footer-instagram' src='../images/instagram.png' />
+        <img className='footer-instagram' src={instagram} />
       </a>
     </footer>
   );
--- Project1/my-app/src/components/Info.js
+++ Project1/my-app/src/components/Info.js
@@ -1,19 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import my_image from '../images/my_image.jpg';
+import email from '../images/email.jpg';
+import linkedin from '../images/linkedin.png';
 
 export default function Info() {
   return (
     <div className='info'>
-      <img className='info-image' src='../images/my_image.jpg' />
+      <img className='info-image' src={my_image} />
       <h1 className='info-name'>Laura Smith</h1>
       <h3 className='info-role'>Frontend Developer</h3>
       <div className='info-buttons'>
         <a href='#'>
-          <img className='info-email' src='../images/email.jpg' />
+          <img className='info-email' src={email} />
         </a>
         <a href='#'>
-          <img className='info-linkedin' src='../images/linkedin.png' />
+          <img className='info-linkedin' src={linkedin} />
         </a>
       </div>
     </div>
